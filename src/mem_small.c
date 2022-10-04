631dadaa80a48c292a7bda970e2cef2ecdebcc18
Youssef Benjelloun <benjelly@ensipc443.ensimag.fr>
2022-10-04 13:13:17 +0200

    bug resolved

--- src/mem_small.c
+++ src/mem_small.c
@@ -4,31 +4,31 @@
  * Ce code est distribué sous la licence GPLv3+.      *
  ******************************************************/
 
-#include <assert.h>
-#include <stdint.h>
 #include "mem.h"
 #include "mem_internals.h"
+#include <assert.h>
+#include <stdint.h>
 
-void * emalloc_small(unsigned long size) {
-    // appelle la fonction de marquage en lui donnant l'adresse de l'element
-    // sa taille CHUNCKSIZE et le type SMALL_KIND
-        //= (char *)arena.chunkpool;
-    if (arena.chunkpool  == NULL) {
-        unsigned long block_size = mem_realloc_small();
-        void ** addr = arena.chunkpool;
-        assert(block_size % 96 == 0);
-        for(unsigned long i = 0; i < block_size/96; i++) {
-            *addr = (void *)((char *)addr + 96);
-            addr = *addr;
-        }
-        *addr = NULL;
+void *emalloc_small(unsigned long size) {
+  // appelle la fonction de marquage en lui donnant l'adresse de l'element
+  // sa taille CHUNCKSIZE et le type SMALL_KIND
+  //= (char *)arena.chunkpool;
+  if (arena.chunkpool == NULL) {
+    unsigned long block_size = mem_realloc_small();
+    void **addr = arena.chunkpool;
+    assert(block_size % 96 == 0);
+    for (unsigned long i = 0; i < (block_size / 96 - 1); i++) {
+      *addr = (void *)((char *)addr + 96);
+      addr = *addr;
     }
-    void* valid_addr = arena.chunkpool;
-    arena.chunkpool = (char *)arena.chunkpool + 96;
-    return mark_memarea_and_get_user_ptr(valid_addr, CHUNKSIZE, SMALL_KIND);
+    *addr = NULL;
+  }
+  void *valid_addr = arena.chunkpool;
+  arena.chunkpool = *(void **) arena.chunkpool;
+  return mark_memarea_and_get_user_ptr(valid_addr, CHUNKSIZE, SMALL_KIND);
 }
 
 void efree_small(Alloc a) {
-    *(void **) a.ptr = arena.chunkpool;
-    arena.chunkpool = a.ptr;
+  *(void **)a.ptr = arena.chunkpool;
+  arena.chunkpool = a.ptr;
 }
