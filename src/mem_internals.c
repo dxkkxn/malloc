e5a1595551da0ead928346b36683d9dcd2555a4b
bidaultl <louis.bidault@grenoble-inp.org>
2022-10-05 17:49:55 +0200

    init

--- src/mem_internals.c
+++ src/mem_internals.c
@@ -14,51 +14,47 @@ unsigned long knuth_mmix_one_round(unsigned long in) {
   return in * 6364136223846793005UL % 1442695040888963407UL;
 }
 
-void *mark_memarea_and_get_user_ptr(void *ptr, unsigned long size, MemKind k) {
-  unsigned long *long_ptr = (unsigned long *)ptr;
-  *long_ptr = (unsigned long)size;
-  unsigned long magic = knuth_mmix_one_round((unsigned long)long_ptr);
-  long_ptr++;
-  magic &= ~(0b11UL);
-  magic |= k;
-  *long_ptr = magic;
-  long_ptr++;
-  unsigned long *usable_memory = long_ptr;
+void *mark_memarea_and_get_user_ptr(void *ptr, unsigned long size, MemKind k)
+{
+    unsigned long *long_ptr = (unsigned long *)ptr;
+    *long_ptr = (unsigned long)size;
+    unsigned long magic = knuth_mmix_one_round((unsigned long)long_ptr);
+    long_ptr++;
+    magic &= ~(0b111UL);
+    magic |= k;
+    *long_ptr = magic;
+    long_ptr++;
+    unsigned long *usable_memory = long_ptr;
 
-  char * char_ptr = (char *)ptr;
-  char_ptr += size - 16;
-  long_ptr = (unsigned long *)char_ptr;
-  *long_ptr = magic;
-  long_ptr++;
-  *long_ptr = size;
-  return (void *)usable_memory;
+    char *char_ptr = (char *)ptr;
+    char_ptr += size - 16;
+    long_ptr = (unsigned long *)char_ptr;
+    *long_ptr = magic;
+    long_ptr++;
+    *long_ptr = size;
+    return (void *) usable_memory;
 }
 
-Alloc mark_check_and_get_alloc(void *ptr) {
-  unsigned long *long_ptr = (unsigned long *)ptr;
-  long_ptr -= 2;
-  void *start_ptr = (void *)(long_ptr);
-  unsigned long size = *long_ptr;
-  unsigned long magic = *(long_ptr + 1);
-  MemKind kind = magic & 0b11UL;
-  magic &= ~(0b11UL); // pop 2 last bits
-  unsigned long magic_verif = knuth_mmix_one_round((unsigned long)long_ptr);
-  magic_verif &= ~(0b11UL); // pop 2 last bits
-  assert(magic == magic_verif);
-  // verif of last block
-  char * char_ptr = (char *)start_ptr;
-  char_ptr += size - 16;
-  long_ptr = (unsigned long *)char_ptr;
-  unsigned long magic_last = *long_ptr;
-  unsigned long size_last = *(long_ptr + 1);
-  assert(size == size_last);
-  MemKind kind_last = magic_last & 0b11UL;
-  magic_last &= ~(0b11UL); // pop 2 last bits
-  assert(magic_last == magic);
-  assert(kind == kind_last);
-
-  Alloc a = {start_ptr, kind, size};
-  return a;
+Alloc
+mark_check_and_get_alloc(void *ptr)
+{    
+    void *start_ptr = ptr - 16;
+    unsigned long *long_ptr = (unsigned long *) start_ptr;
+    unsigned long size = *long_ptr;
+    long_ptr++;
+    unsigned long magic = *long_ptr;
+    char *char_ptr = (char *)ptr;
+    char_ptr += size - 2*16;
+    long_ptr = (unsigned long *)char_ptr;
+    assert(magic == *long_ptr);
+    long_ptr++;
+    assert(size == *long_ptr);
+    Alloc a = {
+        .ptr = start_ptr,
+        .kind = magic & 0b111UL,
+        .size = size
+    };
+    return a;
 }
 
 unsigned long mem_realloc_small() {
--- src/mem_small.c
+++ src/mem_small.c
@@ -4,31 +4,29 @@
  * Ce code est distribué sous la licence GPLv3+.      *
  ******************************************************/
 
+#include <assert.h>
 #include "mem.h"
 #include "mem_internals.h"
-#include <assert.h>
-#include <stdint.h>
 
-void *emalloc_small(unsigned long size) {
-  // appelle la fonction de marquage en lui donnant l'adresse de l'element
-  // sa taille CHUNCKSIZE et le type SMALL_KIND
-  //= (char *)arena.chunkpool;
-  if (arena.chunkpool == NULL) {
-    unsigned long block_size = mem_realloc_small();
-    void **addr = arena.chunkpool;
-    assert(block_size % 96 == 0);
-    for (unsigned long i = 0; i < (block_size / 96 - 1); i++) {
-      *addr = (void *)((char *)addr + 96);
-      addr = *addr;
+void *
+emalloc_small(unsigned long size)
+{
+    if(arena.chunkpool == NULL){
+        unsigned long count = mem_realloc_small() / CHUNKSIZE;
+        void **ptr = (void **)arena.chunkpool;
+        for(int i = 0; i < count - 1; i++){
+            *ptr = (void *)((char *)ptr + CHUNKSIZE);
+            ptr = *ptr;
+        }
+        *ptr = NULL;
+        return emalloc_small(size);
     }
-    *addr = NULL;
-  }
-  void *valid_addr = arena.chunkpool;
-  arena.chunkpool = *(void **) arena.chunkpool;
-  return mark_memarea_and_get_user_ptr(valid_addr, CHUNKSIZE, SMALL_KIND);
+    void *ptr = arena.chunkpool;
+    arena.chunkpool = *(void **) arena.chunkpool;
+    return mark_memarea_and_get_user_ptr(ptr, CHUNKSIZE, SMALL_KIND);
 }
 
 void efree_small(Alloc a) {
-  *(void **)a.ptr = arena.chunkpool;
-  arena.chunkpool = a.ptr;
+    *(void **)a.ptr = arena.chunkpool;
+    arena.chunkpool = a.ptr;
 }
