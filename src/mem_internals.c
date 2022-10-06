9fa6ae22281525dd3c7cd2d3dfb7a72244e6e30f
Louis Bidault <louis.bidault@grenoble-inp.org>
2022-10-06 16:51:51 +0200

    comments added to internals and small

--- src/mem_internals.c
+++ src/mem_internals.c
@@ -14,19 +14,19 @@ unsigned long knuth_mmix_one_round(unsigned long in) {
   return in * 6364136223846793005UL % 1442695040888963407UL;
 }
 
-void *mark_memarea_and_get_user_ptr(void *ptr, unsigned long size, MemKind k) {
+//write mark and return the first address of usable memory
+void *mark_memarea_and_get_user_ptr(void *ptr, unsigned long size, MemKind k) { 
   unsigned long *long_ptr = (unsigned long *)ptr;
   *long_ptr = (unsigned long)size;
   unsigned long magic = knuth_mmix_one_round((unsigned long)long_ptr);
   long_ptr++;
-  magic &= ~(0b11UL);
-  magic |= k;
+  magic &= ~(0b11UL); //liberation of light weight bits
+  magic |= k; //add memory kind on those
   *long_ptr = magic;
   long_ptr++;
   unsigned long *usable_memory = long_ptr;
-
   char * char_ptr = (char *)ptr;
-  char_ptr += size - 16;
+  char_ptr += size - 16; //point to the ending mark
   long_ptr = (unsigned long *)char_ptr;
   *long_ptr = magic;
   long_ptr++;
@@ -34,13 +34,14 @@ void *mark_memarea_and_get_user_ptr(void *ptr, unsigned long size, MemKind k) {
   return (void *)usable_memory;
 }
 
+//read mark and check its validity then return allocation data 
 Alloc mark_check_and_get_alloc(void *ptr) {
   unsigned long *long_ptr = (unsigned long *)ptr;
-  long_ptr -= 2;
+  long_ptr -= 2; //point to first address of marked memory
   void *start_ptr = (void *)(long_ptr);
   unsigned long size = *long_ptr;
   unsigned long magic = *(long_ptr + 1);
-  MemKind kind = magic & 0b11UL;
+  MemKind kind = magic & 0b11UL; //get memory kind back from magic value
   magic &= ~(0b11UL); // pop 2 last bits
   unsigned long magic_verif = knuth_mmix_one_round((unsigned long)long_ptr);
   magic_verif &= ~(0b11UL); // pop 2 last bits
@@ -54,7 +55,7 @@ Alloc mark_check_and_get_alloc(void *ptr) {
   assert(size == size_last);
   MemKind kind_last = magic_last & 0b11UL;
   magic_last &= ~(0b11UL); // pop 2 last bits
-  assert(magic_last == magic);
+  assert(magic == magic_last);
   assert(kind == kind_last);
 
   Alloc a = {start_ptr, kind, size};
