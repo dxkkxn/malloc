7227b16ce873da1204a0a32b23ef93537b1134c2
Youssef Benjelloun <benjelly@ensipc383.ensimag.fr>
2022-10-06 17:23:26 +0200

    finished

--- src/mem_medium.c
+++ src/mem_medium.c
@@ -43,15 +43,13 @@ void create_new_block(unsigned long index) {
     arena.TZL[index + 1] = *(void **)addr_to_divide;
     void **buddy = (void *)((uint64_t)addr_to_divide ^ (1 << index));
     *(void **)addr_to_divide = buddy;
-    *buddy = arena.TZL[index];
-    arena.TZL[index] = addr_to_divide;
+    *buddy = arena.TZL[index]; // buddy points to the first element in index
+    arena.TZL[index] = addr_to_divide; // addr_to_divide becomes the new head
   }
 }
 
-/*
- */
 void *emalloc_medium(unsigned long size) {
-  assert(size + 32 < LARGEALLOC);
+  assert(size + 32 < LARGEALLOC); // The limit
   assert(size > SMALLALLOC);
   unsigned int index = puiss2(size + 32);
   if (arena.TZL[index] == NULL)
@@ -62,42 +60,36 @@ void *emalloc_medium(unsigned long size) {
   return mark_memarea_and_get_user_ptr(ptr, size + 32, MEDIUM_KIND);
 }
 
-void *get_min_buddy(void *addr, unsigned long size) {
-  void *min_addr;
-  if ((uint64_t)addr < ((uint64_t)addr ^ size)) {
-    min_addr = addr;
-  } else {
-    min_addr = (void *)((uint64_t)addr ^ size);
-  }
-  return min_addr;
-}
-
-void buddy_search(void *addr, unsigned long size) {
-  // if buddy in tzl merge with the buddy recursively and return true
-  // if not return false;
-  // addr = address of the buddy
-  void **curr = arena.TZL[size];
+/*
+ * Search the buddy in arena.TZL[index] if it founds it search the buddy of the
+ * new block recursively
+ */
+void buddy_search(void *addr, unsigned long index) {
+  void **curr = arena.TZL[index];
   void **prev = NULL;
-  void *buddy_addr = (void *)((uint64_t)addr ^ (1 << size));
+  void *buddy_addr = (void *)((uint64_t)addr ^ (1 << index));
   while (curr != NULL) {
     if (curr == buddy_addr) {
       if (prev == NULL) {
-        arena.TZL[size] = *curr;
+        // buddy in the head of linked list 
+        arena.TZL[index] = *curr;
       } else {
+        // gets out the block found
         *prev = *curr;
       }
       void *min_addr =
           (uint64_t)addr < (uint64_t)buddy_addr ? addr : buddy_addr;
-      buddy_search(min_addr, size + 1);
+      // get the smaller adresss and
+      buddy_search(min_addr, index + 1);
       return;
     }
     prev = curr;
     curr = *curr;
   }
   // buddy not in TZL
-  // insert addr in head
-  *(void **)addr = arena.TZL[size];
-  arena.TZL[size] = addr;
+  // insert addr in head at arena.TZL[index]
+  *(void **)addr = arena.TZL[index];
+  arena.TZL[index] = addr;
 }
 void efree_medium(Alloc a) {
   buddy_search(a.ptr, puiss2(a.size));
